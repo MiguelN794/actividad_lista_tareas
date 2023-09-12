2ae1a333279942e1c0c2b0248694520bbc36c256
Miguel N Saleme <119358104+MiguelN794@users.noreply.github.com>
2023-09-12 11:57:38 -0300

    v2.2 - Correccion de errores

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function agregarTarea() {
 
         const botonCompletar = document.createElement("button");
         botonCompletar.textContent = "Completar";
-        botonCompletar.className = "completed";
+        botonCompletar.className = "completed gris";
         botonCompletar.onclick = function () {
             marcarCompletada(nuevaTarea);
         }
@@ -30,9 +30,64 @@ function agregarTarea() {
 
 }
 
+function moverATareasCompletadas(tarea) {
+    const listaTareasCompletadas = document.getElementById("listaTareasCompletadas");
+    listaTareasCompletadas.appendChild(tarea);
+
+    const botonEliminar = document.createElement("button");
+    botonEliminar.textContent = "Eliminar";
+    botonEliminar.className = "delete";
+    botonEliminar.onclick = function () {
+        listaTareas.removeChild(listaTareasCompletadas);
+    }
+}
+
+// Funcion para ocultar la lista de tareas pendientes
+function ocultarListaPendientes() {
+    const listaTareas = document.getElementById("listaTareas");
+    listaTareas.style.display = "none";
+}
+
+// Funcion para mostrar la lista de tareas pendientes
+function mostrarListaPendientes() {
+    const listaTareas = document.getElementById("listaTareas");
+    listaTareas.style.display = "block";
+}
+
+// Funcion para ocultar la lista de tareas completadas
+function ocultarListaCompletadas() {
+    const listaTareasCompletadas = document.getElementById("listaTareasCompletadas");
+    listaTareasCompletadas.style.display = "none";
+}
+
+// Funcion para mostrar la lista de tareas completadas
+function mostrarListaCompletadas() {
+    const listaTareasCompletadas = document.getElementById("listaTareasCompletadas");
+    listaTareasCompletadas.style.display = "block";
+}
+
+function mostrarCompletadas() {
+    ocultarListaPendientes();
+    mostrarListaCompletadas();
+}
+
+function mostrarPendientes() {
+    ocultarListaCompletadas();
+    mostrarListaPendientes();
+}
+
+// ...
+
+
+
+
 // Funcion para marcar una tarea como completada.
 function marcarCompletada(tarea) {
     tarea.classList.toggle("completada");
+    const botonCompletar = tarea.querySelector(".completed");
+    botonCompletar.classList.toggle("verde");
+    botonCompletar.textContent = botonCompletar.classList.contains("verde") ? "Completada" : "Completar";
+    moverATareasCompletadas(tarea);
 }
 
 // Agregar evento de clic a las tareas para marcarlas como completadas.
@@ -46,3 +101,10 @@ listaTareas.addEventListener("click", function (e) {
 // Agregar evento de clic al boton "Agregar"
 const botonAgregar = document.getElementById("agregar");
 botonAgregar.addEventListener("click", agregarTarea);
+
+// Agregar evento de clic a los botones "Mostrar Completadas" y "Mostrar Pendientes"
+const botonMostrarCompletadas = document.getElementById("mostrarCompletadas");
+botonMostrarCompletadas.addEventListener("click", mostrarCompletadas);
+
+const botonMostrarPendientes = document.getElementById("mostrarPendientes");
+botonMostrarPendientes.addEventListener("click", mostrarPendientes);
